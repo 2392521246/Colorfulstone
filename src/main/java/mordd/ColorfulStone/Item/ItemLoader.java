bd56ea93aa0d2e830f24b096cac6067ade7528d2
mordds <46252331+mordds@users.noreply.github.com>
2020-05-24 14:25:13 +0800

    Create ItemLoader.java

---
+++ src/main/java/mordd/ColorfulStone/Item/ItemLoader.java
@@ -0,0 +1,12 @@
+package mordd.ColorfulStone.Item;
+
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.common.util.EunmHelper;
+import net.minecraft.Item.item;
+
+public class ItemLoader {
+
+
+
+
+}
