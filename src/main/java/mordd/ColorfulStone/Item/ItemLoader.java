11ccdd02c8c23cdc8d1ddcdcf37f5e7139798f7a
mordds <46252331+mordds@users.noreply.github.com>
2020-08-04 22:14:46 +0800

    Update ItemLoader.java

--- src/main/java/mordd/ColorfulStone/Item/ItemLoader.java
+++ src/main/java/mordd/ColorfulStone/Item/ItemLoader.java
@@ -1,12 +1,21 @@
 package mordd.ColorfulStone.Item;
 
-import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.common.util.EunmHelper;
-import net.minecraft.Item.item;
+import net.minecraftforge.fml.common.mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.item.Item;
 
+@Mod.EventBusSubscriber(modid = "ColorfulStone")
 public class ItemLoader {
+    // WIP
+    public static Item RegistryList[];
+    public static Item redIngot;
 
-
-
-
+    @SubscribeEvent
+    public static void registerItem(RegistryEvent.Register<Item> event) {
+    event.getRegistry().registerAll(RegistryList);
+    }
+    private void define(){
+    
+    }
 }
+
