54a0d57a12bf755e7f05b26a1b6bfd15bc3f9e91
mordds <46252331+mordds@users.noreply.github.com>
2020-08-05 10:22:08 +0800

    Update ItemLoader.java

--- src/main/java/mordd/ColorfulStone/Item/ItemLoader.java
+++ src/main/java/mordd/ColorfulStone/Item/ItemLoader.java
@@ -1,21 +1,27 @@
 package mordd.ColorfulStone.Item;
 
-import net.minecraftforge.fml.common.mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.mod.EventBusSubscriber;
 
 @Mod.EventBusSubscriber(modid = "ColorfulStone")
 public class ItemLoader {
-    // WIP
-    public static Item RegistryList[];
+    //WIP
     public static Item redIngot;
-
+    public static final CreativeTabs COLORFUL_STONE = new CreativeTabs("example_tab") {
+    @Override
+    public ItemStack createIcon() {
+        return new ItemStack(redIngot);
+        }
+    };
     @SubscribeEvent
     public static void registerItem(RegistryEvent.Register<Item> event) {
-    event.getRegistry().registerAll(RegistryList);
+    define();
+    event.getRegistry().registerAll(new Item[]{redIngot});
     }
     private void define(){
-    
+    redIngot = new Item().setRegistryName("colorfulstone:.red_ingot").setCreativeTab(COLORFUL_STONE);
     }
 }
 
