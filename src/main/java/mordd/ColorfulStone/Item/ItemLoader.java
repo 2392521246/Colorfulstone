0b870e36392af0a60aa30719dcf16d54408072ad
Mordd <46252331+mordds@users.noreply.github.com>
2020-08-06 21:54:41 +0800

    Add files via upload

--- src/main/java/mordd/ColorfulStone/Item/ItemLoader.java
+++ src/main/java/mordd/ColorfulStone/Item/ItemLoader.java
@@ -1,17 +1,18 @@
 package mordd.ColorfulStone.Item;
 
+import mordd.ColorfulStone.ColorfulStone;
+import net.minecraft.creativetab.*;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.creativetab.*;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 
 @EventBusSubscriber(modid = "ColorfulStone")
 public class ItemLoader {
     public static Item redIngot;
-    public static final CreativeTabs COLORFUL_STONE = new CreativeTabs("colorful_stone") {
+    public static final CreativeTabs COLORFUL_STONE = new CreativeTabs("colorfulstone") {
     @Override
     public ItemStack getTabIconItem() {
         return new ItemStack(redIngot);
@@ -21,6 +22,9 @@ public class ItemLoader {
     public static void registerItem(RegistryEvent.Register<Item> event) {
     define();
     event.getRegistry().registerAll(new Item[]{redIngot});
+    for (int i=0;i<50;i++){
+    ColorfulStone.logger.error("The Event has Loaded!");
+    }
     }
     private static void define(){
      redIngot = new Item().setRegistryName("colorfulstone:red_ingot").setCreativeTab(COLORFUL_STONE).setUnlocalizedName("colorfulstone.redIngot");
--- src/main/java/mordd/ColorfulStone/Item/ModelMapper.java
+++ src/main/java/mordd/ColorfulStone/Item/ModelMapper.java
@@ -1,5 +1,6 @@
 package mordd.ColorfulStone.Item;
 
+import mordd.ColorfulStone.ColorfulStone;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.client.event.ModelRegistryEvent;
@@ -12,6 +13,9 @@ public final class ModelMapper {
     @SubscribeEvent
     public static void onModelReg(ModelRegistryEvent event) {
         ModelLoader.setCustomModelResourceLocation(ItemLoader.redIngot, 0, new ModelResourceLocation(ItemLoader.redIngot.getRegistryName(), "inventory"));
+        for (int i=0;i<50;i++){
+        ColorfulStone.logger.error("The Event has Loaded! A");
+        }
     }
 }
 
