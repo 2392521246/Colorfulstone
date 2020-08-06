3c15af3f25b49e37386a4f3acad92ad9f42b77d2
Mordd <46252331+mordds@users.noreply.github.com>
2020-08-06 13:41:59 +0800

    Add files via upload

--- src/main/java/mordd/ColorfulStone/Item/ModelMapper.java
+++ src/main/java/mordd/ColorfulStone/Item/ModelMapper.java
@@ -3,7 +3,7 @@ package mordd.ColorfulStone.Item;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.mod.EventBusSubscriber;
 
 @Mod.EventBusSubscriber(value = Side.CLIENT, modid = "colorfulstone")
