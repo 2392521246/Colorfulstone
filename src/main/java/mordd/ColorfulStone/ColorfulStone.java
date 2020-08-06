ef8e3e77dc99d89a4a18f6787386abac0b5859d0
Mordd <46252331+mordds@users.noreply.github.com>
2020-08-06 14:22:22 +0800

    Add files via upload

--- src/main/java/mordd/ColorfulStone/ColorfulStone.java
+++ src/main/java/mordd/ColorfulStone/ColorfulStone.java
@@ -12,7 +12,7 @@ public class ColorfulStone{
   public static final String MODID = "colorfulstone";
   public static final String NAME = "ColorfulStone";
   public static final String VERSION = "0.1.0";
-  @Instance(MODID)
+  @Mod.Instance(MODID)
   public static ColorfulStone instance;
 
   public ColorfulStone(){
