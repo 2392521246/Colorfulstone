8adf788d81223b415dd0392c84d58fc99b122a3e
Mordd <46252331+mordds@users.noreply.github.com>
2020-08-06 16:29:10 +0800

    Add files via upload

---
+++ src/main/java/mordd/ColorfulStone/ItemLoader.java
@@ -0,0 +1,28 @@
+package mordd.ColorfulStone.Item;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.creativetab.*;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.event.RegistryEvent;
+
+
+@EventBusSubscriber(modid = "ColorfulStone")
+public class ItemLoader {
+    public static Item redIngot;
+    public static final CreativeTabs COLORFUL_STONE = new CreativeTabs("colorful_stone") {
+    @Override
+    public ItemStack getTabIconItem() {
+        return new ItemStack(redIngot);
+        }
+    };
+    @SubscribeEvent
+    public static void registerItem(RegistryEvent.Register<Item> event) {
+    define();
+    event.getRegistry().registerAll(new Item[]{redIngot});
+    }
+    private static void define(){
+    redIngot = new Item().setRegistryName("colorfulstone:red_ingot").setCreativeTab(COLORFUL_STONE).setTranslationKey("colorfulstone.redIngot");
+    }
+}
